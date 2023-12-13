407a67afcc9c5857962d771364553305c03de3cc
Harmeet Singh <harmeet_singh1@softprodigy.com>
2023-12-13 12:06:10 +0530

    navbar make visible in loginpage.

--- React_frontend/src/App.js
+++ React_frontend/src/App.js
@@ -3,21 +3,17 @@ import NavbarComponent from "./components/navbar";
 import Router_view from './routes/index'
 import { useDispatch } from "react-redux";
 import { checkAuth } from "./store/login";
-import { useLocation } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
   const dispatch = useDispatch()
-  const location = useLocation()
   useEffect(() => {
     dispatch(checkAuth())
   }, [])
   return (
     <div ><ToastContainer />
-      <header className="App-header">
-        {location.pathname != '/login' ?
-          <NavbarComponent /> : ""
-        }
+      <header className="App-header">       
+        <NavbarComponent /> 
       </header>
       <Router_view />
     </div>
